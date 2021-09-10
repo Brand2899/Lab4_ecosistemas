7f72857dc848dad49da2adbad98b7ac167aacf28
Brand28 <alejandrobrand28@gmail.com>
2021-09-10 15:14:50 -0500

    Segunda página creada

--- app/src/main/java/com/laboratorio4/MainActivity.java
+++ app/src/main/java/com/laboratorio4/MainActivity.java
@@ -3,12 +3,35 @@ package com.laboratorio4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText IPnumber1;
+    private EditText IPnumber2;
+    private EditText IPnumber3;
+    private EditText IPnumber4;
+
+    private Button bnPing;
+    private Button bnSearch;
+
+    private TextView txtMyIP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IPnumber1 = findViewById(R.id.IPnumber1);
+        IPnumber2 = findViewById(R.id.IPnumber2);
+        IPnumber3 = findViewById(R.id.IPnumber3);
+        IPnumber4 = findViewById(R.id.IPnumber4);
+
+        bnPing = findViewById(R.id.bnPing);
+        bnSearch = findViewById(R.id.bnSearch);
+
+        txtMyIP = findViewById(R.id.txtMyIP);
     }
 }
